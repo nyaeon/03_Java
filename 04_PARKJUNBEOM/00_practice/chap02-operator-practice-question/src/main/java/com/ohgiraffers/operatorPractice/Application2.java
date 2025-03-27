b34a0993d9d405a5b860070fabf99cf9ac8302b1
junbem <junbem0728@naver.com>
2025-03-27 23:50:03 +0900

    0327 00_practice chap02 완료

--- 04_PARKJUNBEOM/00_practice/chap02-operator-practice-question/src/main/java/com/ohgiraffers/operatorPractice/Application2.java
+++ 04_PARKJUNBEOM/00_practice/chap02-operator-practice-question/src/main/java/com/ohgiraffers/operatorPractice/Application2.java
@@ -3,7 +3,9 @@ package com.ohgiraffers.operatorPractice;
 public class Application2 {
 
     public static void main(String[] args) {
-
+        int age = 19;
+        String result = (age <= 13) ? "어린이": (age <= 19) ? "청소년" : "성인";
+        System.out.println(result);
         /*
          * 임의의 정수 한개를 선언하고 해당 숫자가 13세 이하이면 어린이, 13세초과 ~ 19세 이하이면
          * 청소년, 19세 초과이면 성인이라고 출력되도록 작성하세요
