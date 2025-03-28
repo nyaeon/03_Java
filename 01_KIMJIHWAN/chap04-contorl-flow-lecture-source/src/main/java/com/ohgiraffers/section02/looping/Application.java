fb2c543364f61d2518edcd87d2f848f527d8f382
FerryLa <rlawl4240@gmail.com>
2025-03-28 23:38:36 +0900

    250328 실습

---
+++ 01_KIMJIHWAN/chap04-contorl-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application {
+
+    public static void main(String[] args) {
+        A_for a = new A_for();
+        a.testSimpleForstatement();
+
+        A_nestedFor b = new A_nestedFor();
+//        b.printGugudanFromTowToNine();
+
+//        b.printStarInputRowTimes();
+
+//        b.printStr
+
+        B_while c = new B_while();
+//        c.testSimpplewhileStatement();
+//        c.testWhileExample();
+
+        D_doWhile d = new D_doWhile();
+//        d.testSimpleDowhileStatement();
+//        d.testDoWhile();
+    }
+}
