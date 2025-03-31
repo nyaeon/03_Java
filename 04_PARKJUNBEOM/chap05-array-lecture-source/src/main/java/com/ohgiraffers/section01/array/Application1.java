40bec2df9ac0ad4b0dff2d5a2647a9d6ab89681b
junbem <junbem0728@naver.com>
2025-03-31 14:05:29 +0900

    커밋

---
+++ 04_PARKJUNBEOM/chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Application1.java
@@ -0,0 +1,45 @@
+package com.ohgiraffers.section01.array;
+
+import java.util.Arrays;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int num4 = 40;
+        int num5 = 50;
+
+        int sum = 0;
+
+        sum += num1;
+        sum += num2;
+        sum += num3;
+        sum += num4;
+        sum += num5;
+
+        System.out.println("sum = " + sum);
+
+        int[] arr = new int[5]; // 배열엔 무조건 값을 채워줘야함 [0][0][0][0][0]
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+
+        //배열 초기화 (값은 보이지 않음)
+        for(int i = 0; i < 5; i++)  {
+            arr[i] = (i + 1) * 10;
+        }
+
+        //반복문을 활용해서 초기화된 값 확인
+        for(int i = 0; i < 5; i++)  {
+            System.out.println(arr[i]);
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int sum2 = 0;
+        for(int i = 0; i < 5; i++)  {
+            sum2  += arr[i];
+        }
+        System.out.println("sum2 = " + sum2);
+    }
+}
---
+++ 04_PARKJUNBEOM/chap05-array-lecture-source/src/main/java/com/ohgiraffers/section03/copy/Application2.java
@@ -0,0 +1,57 @@
+package com.ohgiraffers.section03.copy;
+
+import java.util.Arrays;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        /*
+        * 깊은 복사를 하는 방법은 4가지가 있다.
+        * 1. for문을 이용한 동일한 인덱스 값을 일일이 복사
+        * 2. Object의 clone()을 이용한 복사(사용 빈도 높음)
+        * 3. System의 arraycopy()를 이용한 복사
+        * 4. Arrays의 copyOf()를 이용한 복사
+         */
+
+        int[] originArr = new int[]{1, 2, 3, 4};
+        print(originArr, desc "원본");
+        // 1. for문 활용
+        int[] copyArr1 = new int[originArr.length];
+        for(int i = 0; i < originArr.length; i++) {
+            copyArr1[i] = originArr[i];
+        }
+        print(copyArr1, desc "for문을 활용한 사본");
+        copyArr1[0] = 100;
+        print(originArr, desc"확인용");
+        print(copyArr1, desc"확인용");
+
+        // 2. clone() 활용
+        int[] copyArr2 = originArr.clone();
+        print(copyArr2, desc " clone()을 활용한 사본");
+
+        // 3. arraycopy()를 이용한 복사
+        int[] copyArr3 = new int[originArr.length + 3]; // 원본보다 3크기가 더 큰 배열 할당
+
+        //          원본배열, 복사를 시작할 인덱스, 복사본 배열, 복사를 시작할 인덱스, 복사할 길이
+        System.arraycopy(originArr, srcPos: 0, copyArr3, destPos: 0, originArr.length);
+        print(copyArr3, desc "arraycopy()fmf ghkfdydgks tkqhs");
+
+        // 4. copyOf()를 이용한 복사(원본의 처음부터만 가능)
+        int [] copyArr4 = Arrays.copyOf(originArr, newLength: 3);
+        print(copyArr4, desc "copyOf")
+
+
+    }
+
+    /***
+     * 전달 받은 배열의 정보추력용 메소드
+     * @param arr 출력할 배열의 주소값
+     * @param desc 출력할 명
+     */
+
+    public static void print(int[] arr, String desc) {
+        System.out.printf("============= %s ========================\n", desc);
+        System.out.println("넘어온 배열의 hashCode 값: " + arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
