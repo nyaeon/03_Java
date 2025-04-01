931a5d8d3c28922254d1a825905ac832ac3afa7a
junga970 <junga2940@gmail.com>
2025-04-01 18:11:13 +0900

    0401 실습파일 및 과제제출

---
+++ 11_LEEJUNGA/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/finalkeyword/Application.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section05.finalkeyword;
+
+import java.util.Arrays;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
