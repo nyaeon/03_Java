0bc5dfc518d193b4b804416198d929311f233e2a
hyeblee <hyeb1224@naver.com>
2025-04-03 10:46:43 +0900

    chap03-method-and-api-practice-question

--- 15_HYEBINLEE/00_Practice/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/normal/Application.java
+++ 15_HYEBINLEE/00_Practice/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/normal/Application.java
@@ -1,11 +1,16 @@
 package com.ohgiraffers.section01.level01.normal;
 
+import java.util.Random;
+
 public class Application {
 
     /* 실행용 메소드 */
     public static void main(String[] args) {
 
         /* RamdomMaker 클래스의 메소드를 호출해서 실행 */
+        RandomMaker randomMaker = new RandomMaker();
+        RandomMaker.rockPaperScissors();
+        RandomMaker.tossCoin();
     }
 
 }
