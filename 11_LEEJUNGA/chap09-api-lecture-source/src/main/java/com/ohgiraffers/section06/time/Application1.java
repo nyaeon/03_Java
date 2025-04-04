f2232dee483d25d13c4499681fd8928e25a2b934
junga970 <junga2940@gmail.com>
2025-04-04 20:38:11 +0900

    0404 실습파일 및 과제제출

---
+++ 11_LEEJUNGA/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section06/time/Application1.java
@@ -0,0 +1,28 @@
+package com.ohgiraffers.section06.time;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        LocalTime timeNow = LocalTime.now();
+        LocalTime timeNow2 = LocalTime.of(16, 48, 20);
+        System.out.println("timeNow: " + timeNow);
+        System.out.println("timeNow2: " + timeNow2);
+
+        LocalDate dateNow = LocalDate.now();
+        LocalDate dateNow2 = LocalDate.of(2025, 4, 5);
+        System.out.println("dateNow: " + dateNow);
+        System.out.println("dateNow2: " + dateNow2);
+
+        ZonedDateTime zonedDateTimeNow = ZonedDateTime.now();
+        System.out.println("zonedDateTimeNow: " + zonedDateTimeNow);
+        ZonedDateTime zonedDateTimeOf =
+                ZonedDateTime.of(dateNow2, timeNow2, ZoneId.of("Asia/Seoul"));
+        System.out.println("zonedDateTimeOf: " + zonedDateTimeOf);
+    }
+}
