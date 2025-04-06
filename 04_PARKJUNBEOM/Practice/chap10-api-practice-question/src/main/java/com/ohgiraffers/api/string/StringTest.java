8acd69be25270167f30e31c82059d195635aea38
junbem <junbem0728@naver.com>
2025-04-06 23:31:38 +0900

    chap10 practice 커밋

---
+++ 04_PARKJUNBEOM/Practice/chap10-api-practice-question/src/main/java/com/ohgiraffers/api/string/StringTest.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.api.string;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+
+        StringProcess sp = new StringProcess();
+        String result = sp.preChar("hello world");
+        System.out.println("String 후 : " + result);
+
+        int num = sp.charSu("application car cable", 'c');
+        System.out.println(num);
+    }
+}
