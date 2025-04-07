e849efbb0769aa018737dd82a94a637e086af3bf
Berry-mas <junseo9145@gmail.com>
2025-04-07 17:49:08 +0900

    after lecture 20250407

---
+++ 05_PARKJUNSEO/chap12-io-lecture-source/src/main/java/com/ohgiraffers/section02/stream/Application2.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.section02.stream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Application2 {
+    public static void main(String[] args) {
+        /*
+         * FileReader
+         * FileInputStream과 사용하는 방법이 거의 동일하다.
+         * 단, byte단위가 아닌 character 단위로 읽어들이는 부분이 차이점이다.
+         * 따라서 2바이트이든 3바이트이든 글자 단위로 읽어오기 때문에 한글을 정상적으로 읽어올 수 있다.
+         * */
+
+        // 이렇게 하면 알아서 꺼짐 (finally 안해도)
+        try (FileReader fr = new FileReader("src/main/java/com/ohgiraffers/section02/stream/testReader.txt");) {
+
+//            int value;
+//
+//            while ((value = fr.read()) != -1) {
+//                System.out.print((char) value);
+//            }    // 문자 기반 출력
+            int size = ((int) new File("src/main/java/com/ohgiraffers/section02/stream/testReader.txt").length());
+            System.out.println("size = " + size);
+            char[] carr = new char[size];
+
+            fr.read(carr);
+
+            for (int i = 0; i < carr.length; i++) {
+                System.out.print(carr[i]);
+            }    // 바이트 기반 출력
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
