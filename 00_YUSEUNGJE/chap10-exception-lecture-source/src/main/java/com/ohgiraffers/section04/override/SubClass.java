71b32579acc751465572c863670c408a98be432f
seungje_yu <coffit23@gmail.com>
2025-04-07 15:51:04 +0900

    six period end

--- 00_YUSEUNGJE/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section04/override/SubClass.java
+++ 00_YUSEUNGJE/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section04/override/SubClass.java
@@ -19,7 +19,7 @@ public class SubClass extends SuperClass {
 //    @Override
 //    public void method() throws Exception {}
 
-    // 부모의 예외처리 클래스보다 더 하위에 있는 예외(즉, 더 구체적인 예외상황)인 경우에는 오버라이딩 할 수 있다.
+    // 부모의 예외처리 클래스보다 더 하위에 있는 예외(즉, 더 구체|적인 예외상황)인 경우에는 오버라이딩 할 수 있다.
     @Override
     public void method() throws FileNotFoundException {}
 }
