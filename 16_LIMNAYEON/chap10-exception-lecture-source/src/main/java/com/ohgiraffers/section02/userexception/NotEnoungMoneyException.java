dcbd37135adc30245a5ded17af6e7da83addb037
nayeon <nyaooe@gmail.com>
2025-04-07 17:49:39 +0900

    25-04-07 실습

---
+++ 16_LIMNAYEON/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/NotEnoungMoneyException.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section02.userexception;
+
+public class NotEnoungMoneyException extends Exception{
+
+    public NotEnoungMoneyException() {
+    }
+
+    public NotEnoungMoneyException(String message){
+        super(message);
+    }
+}
