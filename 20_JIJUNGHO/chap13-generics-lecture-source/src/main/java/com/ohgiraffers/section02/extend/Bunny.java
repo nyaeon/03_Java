c37d0e2949e81b18584ce7fc5f7b109bd295d59f
jungho <kiik52@naver.com>
2025-04-08 12:39:24 +0900

    - 제네릭스 와일드카드

---
+++ 20_JIJUNGHO/chap13-generics-lecture-source/src/main/java/com/ohgiraffers/section02/extend/Bunny.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section02.extend;
+
+public class Bunny extends Rabbit {
+
+    @Override
+    public void cry() {
+        System.out.println("바니바니 바니바니 당근🥕 당근🥕 ⬆️⬇️⬆️⬇️");
+    }
+}
