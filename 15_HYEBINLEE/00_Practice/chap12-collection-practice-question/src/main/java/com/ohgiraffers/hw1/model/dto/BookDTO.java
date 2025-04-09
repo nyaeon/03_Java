02eda78b72def501f7c74d3666eb2f9fb536aa57
hyeblee <hyeb1224@naver.com>
2025-04-09 15:45:24 +0900

    feat: BookDTO 생성

---
+++ 15_HYEBINLEE/00_Practice/chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw1/model/dto/BookDTO.java
@@ -0,0 +1,57 @@
+package com.ohgiraffers.hw1.model.dto;
+
+public class BookDTO {
+  int bNo;
+  int cateogry;
+  String title;
+  String author;
+
+  public BookDTO() {}
+  public BookDTO(int cateogry, String title, String author) {
+    this.cateogry = cateogry;
+    this.title = title;
+    this.author = author;
+  }
+
+  @Override
+  public String toString() {
+    return "BookDTO{" +
+        "bNo=" + bNo +
+        ", cateogry=" + cateogry +
+        ", title='" + title + '\'' +
+        ", author='" + author + '\'' +
+        '}';
+  }
+
+  public int getbNo() {
+    return bNo;
+  }
+
+  public void setbNo(int bNo) {
+    this.bNo = bNo;
+  }
+
+  public int getCateogry() {
+    return cateogry;
+  }
+
+  public void setCateogry(int cateogry) {
+    this.cateogry = cateogry;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+}
