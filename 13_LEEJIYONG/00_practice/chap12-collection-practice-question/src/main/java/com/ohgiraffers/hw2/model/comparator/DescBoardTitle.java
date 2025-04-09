37fd2b0a317e935c1c06a3e0530c1c92be7b3958
gyongcode <gyongcode@gmail.com>
2025-04-09 17:40:25 +0900

    chapter 12 과제

---
+++ 13_LEEJIYONG/00_practice/chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw2/model/comparator/DescBoardTitle.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.hw2.model.comparator;
+
+import com.ohgiraffers.hw2.model.dto.BoardDTO;
+import java.util.Comparator;
+
+public class DescBoardTitle implements Comparator<BoardDTO> {
+
+    @Override
+    public int compare(BoardDTO o1, BoardDTO o2) {
+        return o2.getBoardTitle().compareTo(o1.getBoardTitle());
+    }
+}
