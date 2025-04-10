e22b7b69946c0e228897a68af62d4f912e269424
Berry-mas <junseo9145@gmail.com>
2025-04-10 17:50:02 +0900

    20250410 수업 종료 커밋

---
+++ 05_PARKJUNSEO/chap12-io-lecture-source/src/main/java/com/ohgiraffers/section04/testapp/aggregate/BloodType.java
@@ -0,0 +1,6 @@
+package com.ohgiraffers.section04.testapp.aggregate;
+
+public enum BloodType {
+    // C형은 절대 들어올 수 없음 (String이 아닌 enum 타입으로 한 효과)
+    A, AB, B, O;
+}
---
+++ 05_PARKJUNSEO/homework_java_PARKJUNSEO/chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw2/model/comparator/DescBoardTitle.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.hw2.model.comparator;
+
+import com.ohgiraffers.hw2.model.dto.BoardDTO;
+
+public class DescBoardTitle implements Comparable<BoardDTO> {
+
+
+}
