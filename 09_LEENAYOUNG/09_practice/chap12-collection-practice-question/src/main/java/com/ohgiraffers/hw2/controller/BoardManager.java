d6006720b1932987d70e978c92f4c7423f796d4f
nayoung_lee <skdud0694@gmail.com>
2025-04-10 17:49:27 +0900

    2025-04-10 실습 파일 제출

--- 09_LEENAYOUNG/09_practice/chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw2/controller/BoardManager.java
+++ 09_LEENAYOUNG/09_practice/chap12-collection-practice-question/src/main/java/com/ohgiraffers/hw2/controller/BoardManager.java
@@ -16,8 +16,20 @@ public class BoardManager {
         // 글번호 0으로 초기화
         b.setBoardNo(0);
 
+        // 조회내역이 없는 경우
+        // 조회된 내역이 있는 경우 새로운 리스트 생성 & 글 번호를 1로 변경
+        if (boardList.isEmpty()) {
+            boardList = new ArrayList<>();
+            b.setBoardNo(1);
+        } else {
+            // 조회내역이 있는 경우
+            // 기존 리스트의 마지막 인덱스의 글번호 조회
+            b.setBoardNo(boardList.get(boardList.size()-1).getBoardNo() + 1);
+        }
+
         // 출력값 결정(successPage)
         boardList.add(b);
+        rp.successPage("게시글 등록 성공!");
     }
 
     public void selectAllList() {}
