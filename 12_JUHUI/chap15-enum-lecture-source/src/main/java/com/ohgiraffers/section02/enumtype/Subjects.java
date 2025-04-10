cddc4ac8550cd86566eca88f5b854dc86cff29d7
juhui <juya202106@naver.com>
2025-04-10 09:51:23 +0900

    20250410 9시타임 enum

---
+++ 12_JUHUI/chap15-enum-lecture-source/src/main/java/com/ohgiraffers/section02/enumtype/Subjects.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section02.enumtype;
+
+public enum Subjects {
+    JAVA,
+    MYSQL,
+    JDBC,
+    HTML,
+    CSS,
+    JAVASCRIPT;
+
+    Subjects() {
+        System.out.println("기본 생성자 호출됨....");
+    }
+}
