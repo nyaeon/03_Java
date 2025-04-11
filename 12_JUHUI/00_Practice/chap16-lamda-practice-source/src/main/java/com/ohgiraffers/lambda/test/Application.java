b7cdc777afd099b45d6b2cf1d1dec6d1edbb9683
juhui <juya202106@naver.com>
2025-04-11 19:43:31 +0900

    20250411 스트림 문제 완료

--- 12_JUHUI/00_Practice/chap16-lamda-practice-source/src/main/java/com/ohgiraffers/lambda/test/Application.java
+++ 12_JUHUI/00_Practice/chap16-lamda-practice-source/src/main/java/com/ohgiraffers/lambda/test/Application.java
@@ -6,9 +6,9 @@ public class Application {
     public static void main(String[] args) {
         App app = new App();
         app.test1();
-        //app.test2();
-        //app.test3();
-        //app.test4();
-        //app.test5();
+        app.test2();
+        app.test3();
+        app.test4();
+        app.test5();
     }
 }
