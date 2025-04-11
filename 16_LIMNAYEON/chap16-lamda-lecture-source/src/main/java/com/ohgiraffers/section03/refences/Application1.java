9ebe3321f80e591e70fb9f58d28b8d849544c77d
nayeon <nyaooe@gmail.com>
2025-04-11 18:30:36 +0900

    25-04-11 실습 & 람다 과제 제출

---
+++ 16_LIMNAYEON/chap16-lamda-lecture-source/src/main/java/com/ohgiraffers/section03/refences/Application1.java
@@ -0,0 +1,54 @@
+package com.ohgiraffers.section03.refences;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /*
+        * [메소드 참조 표현식]
+        * 클래스이름::메소드이름
+        * 참조변수이름::메소드이름
+        * */
+
+        BiFunction<String, String, Boolean> bf = (str1, str2) -> str1.equals(str2);
+        System.out.println(bf.apply("java", "java"));
+
+        BiFunction<String, String, Boolean> biFunction = String::equals;
+        System.out.println(biFunction.apply("java", "java"));
+
+        Consumer<Object> objectConsumer = System.out::println;
+        objectConsumer.accept("나오니?");
+
+        List<String> subjects = new ArrayList<>();
+        subjects.add("java");
+        subjects.add("mysql");
+        subjects.add("jdbc");
+        subjects.add("html");
+        subjects.add("javascript");
+
+        forEach(subjects, System.out::println);
+
+        System.out.println("==============================");
+        subjects.forEach(System.out::println);
+
+        System.out.println("==============================");
+        for(String str : subjects) {
+            System.out.println(str);
+        }
+
+    }
+
+    private static void forEach(List<? extends  Object> list, Consumer<? super Object> consumer) {
+        for(Object o : list) {
+            consumer.accept(o);
+        }
+
+    }
+}
