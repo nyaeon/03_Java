40e55129e2f9323ef7949f0195c4c63d971511eb
juhui <juya202106@naver.com>
2025-04-11 17:14:15 +0900

    람다 과제1 푸는 중

---
+++ 12_JUHUI/00_Practice/chap16-lamda-practice-source/src/main/java/com/ohgiraffers/lambda/test/App.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.lambda.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//App 클래스 정의 - 여기서 test1 ~ 5까지 메소드 구현
+public class App {
+    @FunctionalInterface
+    interface CurrentTimeFormatter {
+        void printCurrentTime();
+    }
+
+    // 현재시각 HH:mm:ss.SSS을 출력하는 람다식을 작성
+    public void test1() {
+        CurrentTimeFormatter formatter = () -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+            System.out.println("현재 시각 :" + sdf.format(new Date()));
+        };
+    }
+    
+    // 로또를 생성하는 람다식을 작성하세요.
+    public void test2() {
+        /* 구현 */
+    }
+    public void test3() {
+        /* 구현 */
+    }
+    public void test4() {
+        /* 구현 */
+    }
+    public void test5() {
+        /* 구현 */
+    }
+}
--- 12_JUHUI/chap17-stream-lecture-source/src/main/java/com/ohgiraffers/section01/intro/Application2.java
+++ 12_JUHUI/chap17-stream-lecture-source/src/main/java/com/ohgiraffers/section01/intro/Application2.java
@@ -3,7 +3,6 @@ package com.ohgiraffers.section01.intro;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Application2 {
 
