3cab5e23e6b0b2cb8b90c96109a82495ccc514c7
nayoung_lee <skdud0694@gmail.com>
2025-04-15 21:04:41 +0900

    2025-04-15 Java 과제 제출

---
+++ 09_LEENAYOUNG/09_practice/chap17-stream-practice-source/src/main/java/com/ohgiraffers/stream/CheckAge.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.stream;
+
+import com.ohgiraffers.stream.dto.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CheckAge {
+
+    public static void main(String[] args) {
+
+        // 문제: User 리스트에서 나이가 20 이상인 사람들의 이름을 리스트로 출력하세요.
+        List<User> users = List.of(
+                new User("Alice", 23),
+                new User("Bob", 17),
+                new User("Charlie", 19),
+                new User("David", 25)
+        );
+
+        List<String> names = users.stream()
+                .filter(user -> user.getAge() >= 20)
+                .map(User::getName)
+                .collect(Collectors.toList());
+        System.out.println(names);
+    }
+}
