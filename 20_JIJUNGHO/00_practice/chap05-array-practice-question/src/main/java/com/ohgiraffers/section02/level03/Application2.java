1435be1b190dc50ac12d138bb87b7f931b783205
jungho <kiik52@naver.com>
2025-04-16 20:07:56 +0900

    배열 문제 풀이 arraycopy

--- 20_JIJUNGHO/00_practice/chap05-array-practice-question/src/main/java/com/ohgiraffers/section02/level03/Application2.java
+++ 20_JIJUNGHO/00_practice/chap05-array-practice-question/src/main/java/com/ohgiraffers/section02/level03/Application2.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.level03;
 
+import java.util.Arrays;
+
 public class Application2 {
 
     public static void main(String[] args) {
@@ -13,5 +15,20 @@ public class Application2 {
         int[] d = new int[10];
 
         // 향상된 for문을 이용하여 위의 a, b, c의 배열의 값을 d의 배열에 합치기한 후 출력
+
+        int copy = 0;
+        System.arraycopy(a, 0, d, copy, a.length);
+        copy += a.length;
+        System.arraycopy(b, 0, d, copy, b.length);
+        copy += b.length;
+        System.arraycopy(c, 0, d, copy, c.length);
+        copy += c.length;
+
+        for (int i : d) {
+            System.out.print(i + " ");
+        }
+
+
+
     }
 }
